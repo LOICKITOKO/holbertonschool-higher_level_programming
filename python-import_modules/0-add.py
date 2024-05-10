d176925ae7f06784bfaf75daf2ae24bb83bec6a6
LOICKITOKO <loicassaelkm@gmail.com>
2024-05-10 13:56:51 +0200

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
-# Define variables a and b
-a = 1
-b = 2
-
-# Import the add function from add_0.py
 from add_0 import add
 
-# Print the result of the addition
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
