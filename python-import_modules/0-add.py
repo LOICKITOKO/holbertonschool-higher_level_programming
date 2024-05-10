b03b7eba809d7a3b82b732d28f1d0214a09aeba1
LOICKITOKO <loicassaelkm@gmail.com>
2024-05-10 21:21:25 +0200

    add task0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+a = 1
+b = 2
 from add_0 import add
 
-if __name__ == "__main__":
-    a = 1
-    b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
