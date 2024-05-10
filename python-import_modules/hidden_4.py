ab14ad5f0376086ae7a3884db7a1c08f431cd443
LOICKITOKO <loicassaelkm@gmail.com>
2024-05-10 14:28:12 +0200

    add a function to print hidden

---
+++ python-import_modules/hidden_4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def my_secret_function():
+    return "This is a secret function!"
+
+def my_other_function():
+    return "This is another function!"
+
+def _hidden_function():
+    return "This function is hidden!"
+
+if __name__ == "__main__":
+    print("This is the main module.")
