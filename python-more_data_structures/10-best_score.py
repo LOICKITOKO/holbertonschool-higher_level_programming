400a9faf0b45a3a6d9f10e09a4cb16c1c8a83acb
LOICKITOKO <loicassaelkm@gmail.com>
2024-05-15 18:45:11 +0200

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,12 @@ def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
 
-    max_score = max(a_dictionary.values())
-    best_students = [key for key, value in a_dictionary.items() if value == max_score]
+    best_key = None
+    best_value = float('-inf')
 
-    return best_students[0]
+    for key, value in a_dictionary.items():
+        if value > best_value:
+            best_key = key
+            best_value = value
+
+    return best_key
