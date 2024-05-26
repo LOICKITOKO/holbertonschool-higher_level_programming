49b233189fe71dbfa63a94d5037f9bb3167d187c
LOICKITOKO <loicassaelkm@gmail.com>
2024-05-26 21:35:24 +0200

    task_01_duck_typing.py

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
+""" File class multiples shapes. """
 from abc import ABC, abstractmethod
-import math
+from math import pi
+
 
 class Shape(ABC):
+
     @abstractmethod
     def area(self):
         pass
@@ -11,29 +13,33 @@ class Shape(ABC):
     @abstractmethod
     def perimeter(self):
         pass
+
 
 class Circle(Shape):
+
     def __init__(self, radius):
-        self.radius = radius
+        self.__radius = radius
 
     def area(self):
-        return math.pi * (self.radius ** 2)
+        return pi * self.__radius ** 2
 
     def perimeter(self):
-        return 2 * math.pi * self.radius
+        return 2 * pi * abs(self.__radius)
+
 
 class Rectangle(Shape):
+
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     def area(self):
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        return (self.__width + self.__height) * 2
+
 
 def shape_info(shape):
-    print(f"Area: {shape.area()}")
-    print(f"Perimeter: {shape.perimeter()}")
-
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
