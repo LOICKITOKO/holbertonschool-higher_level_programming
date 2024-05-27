64d4fe4c809322110a9e612d69812a6821e9dfd3
LOICKITOKO <loicassaelkm@gmail.com>
2024-05-27 12:01:13 +0200

    function that reads a text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-def read_file(filename=""):
 
-        with open(filename, "r", encoding="utf-8") as file:
+
+def read_file(filename=""):
+    '''
+        Open the file in read mode with UTF-8 encoding
+        The with statement ensures that the file is properly closed after use
+    '''
+
+    with open(filename, "r", encoding="utf-8") as file:
+            #Read the entire content of the file and store it in the variable content 
             content = file.read()
+            # Print the content of the file to the standard output
             print(content)
 
