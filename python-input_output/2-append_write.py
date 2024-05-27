d08a0297da267ff2d6e18a5a6aafdb4590a3bda8
LOICKITOKO <loicassaelkm@gmail.com>
2024-05-27 14:30:48 +0200

    2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,6 +5,7 @@
 
 
 def append_write(filename="", text=""):
+    ''' open the file in append mode'''
     with open(filename,"a", encoding="utf-8") as f:
         count = f.write(text)
         return count
