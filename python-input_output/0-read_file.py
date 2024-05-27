dbd579f5ef1ab0d4ed67279953d66413429ea255
LOICKITOKO <loicassaelkm@gmail.com>
2024-05-27 12:24:36 +0200

    function that reads a text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 '''
-Open the file in read mode with UTF-8 encoding
+    Open the file in read mode with UTF-8 encoding
 '''
 
 
 def read_file(filename=""):
-    #read mode with UTF-8 encoding
-        with open(filename, "r", encoding="utf-8") as file:
+    '''read mode with UTF-8 encoding'''
+    with open(filename, "r", encoding="utf-8") as file:
             content = file.read()
             print(content)
