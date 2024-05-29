9b88311fe8d29b2853645d56f27bf65bf518bb4f
LOICKITOKO <loicassaelkm@gmail.com>
2024-05-29 15:45:00 +0200

    task_03_xml.py

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    root = ET.Element("data")
+    for key, value in dictionary.items():
+        child.text = str(value)
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
+def deserialize_from_xml(filename):
+    try:
+        tree = ET.parse(filename)
+        root = tree.getroot()
+        _dict = {}
+        for element in root:
+            _dict[element.tag] = element.text
+            return _dict
+    except Exception as e:
+        print(f"An error occurrend while deserializing from XML: {e}")
+        return None
