c73a81b8a2da673e4a3552d68186664aad38a87f
LOICKITOKO <loicassaelkm@gmail.com>
2024-06-20 13:44:31 +0200

    task3

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,9 +1,2 @@
--- Task: List all tables in a specific database
-
--- Set the database name (replace `dbname` with your actual database name)
-SET @dbname = 'dbname';
-
--- Query to list tables
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = DATABASE();
+-- script that lists all the tables of a database
+SHOW TABLES;
