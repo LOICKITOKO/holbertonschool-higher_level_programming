f4766a1e3977b8e64a71dea1ea4b705abf7125f2
LOICKITOKO <loicassaelkm@gmail.com>
2024-06-20 13:28:40 +0200

    task2

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,6 @@
+-- Task: Delete database hbtn_0c_0 if it exists
+
+-- Attempt to drop the database
+DROP DATABASE IF EXISTS hbtn_0c_0;
+
+-- No need to use SELECT or SHOW statements as per requirements
