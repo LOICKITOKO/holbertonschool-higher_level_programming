207977d60537c8470bfafdf8156bebd56e0d62d6
LOICKITOKO <loicassaelkm@gmail.com>
2024-06-20 13:25:22 +0200

    task1

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,6 @@
+-- Task: Create database hbtn_0c_0 if it doesn't already exist
+
+-- Attempt to create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- No need to use SELECT or SHOW statements as per requirements
