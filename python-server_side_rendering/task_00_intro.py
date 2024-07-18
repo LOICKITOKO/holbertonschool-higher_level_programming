714c8d7128acde57463419e6faf75f8f59edd659
LOICKITOKO <loicassaelkm@gmail.com>
2024-07-18 19:25:34 +0200

    all task

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -2,30 +2,37 @@ import os
 
 def generate_invitations(template, attendees):
     if not isinstance(template, str):
-        print("Error: Template must be a string.")
+        print("Error: Template must be a sting.")
         return
-
     if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
         print("Error: Attendees must be a list of dictionaries.")
         return
-
-    if not template.strip():
+    
+    if not template:
         print("Error: Template is empty, no output files generated.")
         return
-
-    if not attendess:
-        print("No data provided, no output files generated.")
+    if not attendees:
+        print("Error: Nodata provied, no output files generated.")
         return
+    
 
     for index, attendee in enumerate(attendees, start=1):
-        invitation = template.replace("{name}", attendee.get("name", "N/A"))
-        invitation = invitation.replace("{event_title}", attendee.get("event_title", "N/A"))
-        invitation = invitation.replace("{event_date}", attendee.get("event_date", "N/A") if attendee.get("event_date") is not None else "N/A")
-        invitation = invitation.replace("{event_location}", attendee.get("event_location", "N/A"))
-
+        personalized_content = template
+        personalized_content = personalized_content.replace("{name}",attendee.get("name", "N/A"))
+        personalized_content = personalized_content.replace("{event_title}",attendee.get("event_title", "N/A"))
+        personalized_content = personalized_content.replace("{event_date}",attendee.get("event_date", "N/A"))
+        personalized_content = personalized_content.replace("{event_location}",attendee.get("event_location", "N/A"))
+        
         output_filename = f"output_{index}.txt"
 
-        with open(output_filename, "w") as output_file:
-            output_file.write(invitation)
+        if os.path.exists(output_filename):
+            print(f"Error: {output_filename} already exists.")
+            continue
 
-            print(f"Generated {output_filename}")
+        try:
+            with open(output_filename, "w") as file:
+                file.write(personalized_content)
+            print(f"Generated : {output_filename}")
+        except Exception as e:
+            print(f"Error: Unable to write to {output_filename}.")
+            print(e)
